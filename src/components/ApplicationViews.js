a004f235f7bee9bbd331a4b1784b01630849cc88
travisbrigman <travis1000@gmail.com>
2020-11-05 15:57:34 -0600

    building out inital structure, nav, auth, register, etc.

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ApplicationViews = () => {
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          backgroundColor: "lightgoldenrodyellow",
+        }}
+      ></main>
+    </>
+  );
+};
